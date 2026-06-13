d16b0976227fff49062eac607ecf13c3feed065a
Gouravdevrale <116137382+Gouravdevrale@users.noreply.github.com>
2026-06-13 19:40:32 +0530

    Fix formatting of baseURL in axios configuration

--- hrms-frontend/src/api/axios.js
+++ hrms-frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: https://hrms-backend-fj85.onrender.com/api,
+  baseURL: "https://hrms-backend-fj85.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
